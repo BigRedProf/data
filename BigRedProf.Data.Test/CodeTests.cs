4b337a9eaabf2b65c10e4e7242902c73e329a176
Russell Froehlich <BigRedProf@outlook.com>
2022-03-01 17:10:58 -0800

    added unit test for Code class

---
+++ BigRedProf.Data.Test/CodeTests.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace BigRedProf.Data.Test
+{
+	public class CodeTests
+	{
+		#region main tests
+		[Fact]
+		public void ZeroLengthCodesShouldThrow()
+		{
+			Assert.Throws<ArgumentException>(
+				() =>
+				{
+					new Bit();
+				}
+			);
+		}
+
+		[Fact]
+		public void LengthShouldReturnLength()
+		{
+			Assert.Equal(1, new Code(1).Length);
+			Assert.Equal(2, new Code(1, 0).Length);
+			Assert.Equal(3, new Code(1, 0, 1).Length);
+			Assert.Equal(4, new Code(1, 0, 1, 1).Length);
+			Assert.Equal(500, new Code(Enumerable.Repeat(0, 500).Cast<Bit>().ToArray()).Length);
+			Assert.Equal(8192, new Code(Enumerable.Repeat(1, 8192).Cast<Bit>().ToArray()).Length);
+		}
+
+		[Fact]
+		public void IndexerShouldGetCorrectValues()
+		{
+			Code code = new Code(1, 0, 0, 1, 0, 1);
+			Assert.Equal<Bit>(1, code[0]);
+			Assert.Equal<Bit>(0, code[1]);
+			Assert.Equal<Bit>(0, code[2]);
+			Assert.Equal<Bit>(1, code[3]);
+			Assert.Equal<Bit>(0, code[4]);
+			Assert.Equal<Bit>(1, code[5]);
+		}
+
+		[Fact]
+		public void IndexerShouldSetCorrectValues()
+		{
+			Code code = new Code(0, 0, 0, 0, 0, 0);
+
+			code[0] = 0;
+			Assert.Equal<Bit>(0, code[0]);
+
+			code[1] = 1;
+			Assert.Equal<Bit>(1, code[1]);
+
+			code[2] = 0;
+			Assert.Equal<Bit>(0, code[2]);
+
+			code[3] = 1;
+			Assert.Equal<Bit>(1, code[3]);
+
+			code[4] = 0;
+			Assert.Equal<Bit>(0, code[4]);
+
+			code[5] = 1;
+			Assert.Equal<Bit>(1, code[5]);
+		}
+
+		[Fact]
+		public void IndexerShouldChangeValues()
+		{
+			Code code = new Code(1, 0, 0, 1, 0, 1);
+
+			code[0] = 0;
+			Assert.Equal<Bit>(0, code[0]);
+
+			code[1] = 1;
+			Assert.Equal<Bit>(1, code[1]);
+
+			code[2] = 0;
+			Assert.Equal<Bit>(0, code[2]);
+
+			code[3] = 1;
+			Assert.Equal<Bit>(1, code[3]);
+
+			code[4] = 0;
+			Assert.Equal<Bit>(0, code[4]);
+
+			code[5] = 1;
+			Assert.Equal<Bit>(1, code[5]);
+		}
+		#endregion
+
+		#region object tests
+		[Fact]
+		public void ToStringShouldFormatCorrectly()
+		{
+			Code code = new Code(1, 1, 0, 0, 1, 0, 1, 0);
+			Assert.Equal("11001010", code.ToString());
+		}
+		#endregion
+	}
+}
---
+++ BigRedProf.Data/Code.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigRedProf.Data
+{
+	/// <summary>
+	/// A <see cref="Code"/> is an ordered set of bits. The caller is responsible for encoding models into codes, decoding codes into models
+	/// and defining the meaning of codes it uses.
+	/// </summary>
+	public class Code
+	{
+		#region constructors
+		public Code(params Bit[] bits)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		#region properties
+		/// <summary>
+		/// The length, in bits, of the code.
+		/// </summary>
+		public int Length
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the value of a specific <see cref="Bit"/> within the code.
+		/// </summary>
+		/// <param name="offset"></param>
+		/// <returns></returns>
+		public Bit this[int offset]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+		#endregion
+	}
+}
