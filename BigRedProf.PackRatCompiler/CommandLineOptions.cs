4a711549aee5dc46d96134dbecacdc28a4c34d0e
Russell Froehlich <BigRedProf@outlook.com>
2022-06-26 17:40:14 -0700

    added command line options

---
+++ BigRedProf.PackRatCompiler/CommandLineOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommandLine;
+
+namespace BigRedProf.PackRatCompiler
+{
+	public class CommandLineOptions
+	{
+		[Value(0, MetaName = "Input Path", HelpText = "The root directory of the project.")]
+		public string? InputPath { get; set; }
+
+		[Option('o', "outputPath", Required = true, HelpText = "The output directory for the PackRat files.")]
+		public string? OutputPath { get; set; }
+	}
+}
--- BigRedProf.PackRatCompiler/Program.cs
+++ BigRedProf.PackRatCompiler/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using CommandLine;
+
+namespace BigRedProf.PackRatCompiler
+{
+	public class Program
+	{
+		static private void Main(string[] args)
+		{
+			CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+				.WithParsed(RunOptions)
+				.WithNotParsed(HandleParseError);
+		}
+
+		#region event handlers
+		static private void RunOptions(CommandLineOptions options)
+		{
+			Console.WriteLine("Hello, World!");
+
+			if (options.InputPath != null)
+			{
+				Console.Write("Input directory is ");
+				Console.Write(options.InputPath, ConsoleColor.DarkGreen);
+				Console.WriteLine();
+			}
+
+			if (options.OutputPath != null)
+			{
+				Console.Write("Output directory is ");
+				Console.WriteLine(options.OutputPath, ConsoleColor.Red);
+			}
+		}
+
+		static private void HandleParseError(IEnumerable<Error> errors)
+		{
+			Console.WriteLine("Invalid usage.");
+			foreach(Error error in errors)
+			{
+				Console.WriteLine(error.ToString(), ConsoleColor.Red);
+			}
+		}
+		#endregion
+	}
+}
