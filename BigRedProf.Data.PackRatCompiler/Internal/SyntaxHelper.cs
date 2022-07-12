39d79b80e76a1f081bad9b7e43b642440e82546b
Russell Froehlich <BigRedProf@outlook.com>
2022-07-12 06:49:52 -0700

    fixed attribute detection in SyntaxHelper

--- BigRedProf.Data.PackRatCompiler/Internal/SyntaxHelper.cs
+++ BigRedProf.Data.PackRatCompiler/Internal/SyntaxHelper.cs
@@ -3,6 +3,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,16 @@ namespace BigRedProf.Data.PackRatCompiler.Internal
 		{
 			// TODO: handle fully qualified case
 
+			Debug.Assert(candidateAttributeType.Name.EndsWith("Attribute"));
+			string candidateAttributeName = candidateAttributeType.Name.Substring(
+				0,
+				candidateAttributeType.Name.Length - ("Attribute").Length
+			);
+
 			bool result = attributeSyntax.DescendantNodes()
 				.OfType<IdentifierNameSyntax>()
 				.Any(
-					s => s.Identifier.ValueText == candidateAttributeType.Name
+					s => s.Identifier.ValueText == candidateAttributeName
 				);
 
 			return result;
