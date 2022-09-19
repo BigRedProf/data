2ca6fee25756476a51b0d077ed92db0727e1b48a
Russell Froehlich <BigRedProf@outlook.com>
2022-09-19 16:31:24 -0700

    added PackRat.PackNullableModel and PackRat.UnpackNullableModel methods

--- BigRedProf.Data.Test/PackRatTests.cs
+++ BigRedProf.Data.Test/PackRatTests.cs
@@ -1,16 +1,140 @@
+using BigRedProf.Data.Internal.PackRats;
+using BigRedProf.Data.Test._TestHelpers;
 using System;
+using System.IO;
 using Xunit;
 
 namespace BigRedProf.Data.Test
 {
-	public class PackRatTests
+	public class PackRatTests : PackRat<object>
 	{
-		// Nothing to test here since this is an abstract class.
+		#region protected methods
 		[Fact]
-		[Trait("Region", "none")]
-		public void Placeholder()
+		[Trait("Region", "protected methods")]
+		public void PackNullableModel_ShouldThrowWhenWriterIsNull()
 		{
-			Assert.True(true);
+			PackRatTests packRatTests = new PackRatTests();
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRatTests.PackNullableModel<object>(null, "foo", packRatTests);
+				}
+			);
 		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void PackNullableModel_ShouldThrowWhenPackRatIsNull()
+		{
+			CodeWriter writer = new CodeWriter(new MemoryStream());
+			PackRatTests packRatTests = new PackRatTests();
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRatTests.PackNullableModel<object>(writer, null, null);
+				}
+			);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void PackNullableModel_ShouldWorkForNullModels()
+		{
+			MemoryStream writerStream = new MemoryStream();
+			CodeWriter writer = new CodeWriter(writerStream);
+			PackRatTests packRatTests = new PackRatTests();
+
+			packRatTests.PackNullableModel(writer, null, packRatTests);
+
+			writer.Dispose();
+			Stream readerStream = new MemoryStream(writerStream.ToArray());
+			CodeReader reader = new CodeReader(readerStream);
+			Assert.Equal(1, readerStream.Length);
+			Code actualCode = reader.Read(1);
+			Assert.Equal("0", actualCode);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void PackNullableModel_ShouldWorkForNonNullModels()
+		{
+			MemoryStream writerStream = new MemoryStream();
+			CodeWriter writer = new CodeWriter(writerStream);
+			PackRatTests packRatTests = new PackRatTests();
+
+			packRatTests.PackNullableModel(writer, "foo", packRatTests);
+
+			writer.Dispose();
+			Stream readerStream = new MemoryStream(writerStream.ToArray());
+			CodeReader reader = new CodeReader(readerStream);
+			Assert.Equal(1, readerStream.Length);
+			Code actualCode = reader.Read(2);
+			Assert.Equal("11", actualCode);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void UnpackNullableModel_ShouldThrowWhenReaderIsNull()
+		{
+			PackRatTests packRatTests = new PackRatTests();
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRatTests.UnpackNullableModel<object>(null, packRatTests);
+				}
+			);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void UnpackNullableModel_ShouldThrowWhenPackRatIsNull()
+		{
+			CodeReader reader = new CodeReader(new MemoryStream());
+			PackRatTests packRatTests = new PackRatTests();
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRatTests.UnpackNullableModel<object>(reader, null);
+				}
+			);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void UnpackNullableModel_ShouldWorkForNullModels()
+		{
+			CodeReader reader = PackRatTestHelper.CreateCodeReader("0");
+			PackRatTests packRatTests = new PackRatTests();
+
+			object actualModel = packRatTests.UnpackNullableModel(reader, packRatTests);
+
+			Assert.Null(actualModel);
+		}
+
+		[Fact]
+		[Trait("Region", "protected methods")]
+		public void UnpackNullableModel_ShouldWorkForNonNullModels()
+		{
+			CodeReader reader = PackRatTestHelper.CreateCodeReader("11");
+			PackRatTests packRatTests = new PackRatTests();
+
+			object actualModel = packRatTests.UnpackNullableModel(reader, packRatTests);
+
+			Assert.Equal("foo", actualModel);
+		}
+		#endregion
+
+		#region abstract PackRat methods
+		public override void PackModel(CodeWriter writer, object model)
+		{
+			writer.WriteCode("1");
+		}
+
+		public override object UnpackModel(CodeReader reader)
+		{
+			bool isNull = reader.Read(1) == "0";
+			return isNull ? null : "foo";
+		}
+		#endregion
 	}
 }
--- BigRedProf.Data/PackRat.cs
+++ BigRedProf.Data/PackRat.cs
@@ -11,7 +11,60 @@ namespace BigRedProf.Data
 	/// <typeparam name="T">The type of model to pack and unpack.</typeparam>
 	public abstract class PackRat<T>
 	{
-		#region methods
+		#region protected fields
+		/// <summary>
+		/// Packs a nullable model using the specified pack rat.
+		/// </summary>
+		/// <typeparam name="M"></typeparam>
+		/// <param name="writer">The code writer.</param>
+		/// <param name="model">The model.</param>
+		/// <param name="packRat">The pack rat.</param>
+		protected void PackNullableModel<M>(CodeWriter writer, M model, PackRat<M> packRat)
+			where M : new()
+		{
+			if (writer == null)
+				throw new ArgumentNullException(nameof(writer));
+
+			if (packRat == null)
+				throw new ArgumentNullException(nameof(packRat));
+
+			if(model == null)
+			{
+				writer.WriteCode("0");
+			}
+			else
+			{
+				writer.WriteCode("1");
+				packRat.PackModel(writer, model);
+			}
+		}
+
+		/// <summary>
+		/// Unpacks a nullable model using the specified pack rat.
+		/// </summary>
+		/// <typeparam name="M"></typeparam>
+		/// <param name="reader">The code reader.</param>
+		/// <param name="packRat">The pack rat.</param>
+		/// <returns></returns>
+		protected M UnpackNullableModel<M>(CodeReader reader, PackRat<M> packRat)
+			where M : new()
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			if (packRat == null)
+				throw new ArgumentNullException(nameof(packRat));
+
+			M model = default;
+			bool isNull = reader.Read(1) == "0";
+			if(!isNull)
+				model = packRat.UnpackModel(reader);
+
+			return model;
+		}
+		#endregion
+
+		#region abstract methods
 		/// <summary>
 		/// Packs a model into a <see cref="Code"/>.
 		/// </summary>
