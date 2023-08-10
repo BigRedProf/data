cd0896af6ecc5651ac17e038edafbb9b6903e946
Russell Froehlich <BigRedProf@outlook.com>
2023-08-10 16:59:59 -0700

    added Int64PackRat with ChatGPT-generated unit tests

---
+++ BigRedProf.Data.Test/Internal/PackRats/Int64PackRatTests.cs
@@ -0,0 +1,84 @@
+using BigRedProf.Data.Internal.PackRats;
+using BigRedProf.Data.Test._TestHelpers;
+using System;
+using Xunit;
+
+namespace BigRedProf.Data.Test
+{
+	public class Int64PackRatTests
+	{
+		#region PackRat methods
+		[Fact]
+		[Trait("Region", "PackRat methods")]
+		public void PackModel_ShouldThrowWhenWriterIsNull()
+		{
+			PiedPiper piedPiper = new PiedPiper();
+
+			Int64PackRat packRat = new Int64PackRat(piedPiper);
+			int model = 43;
+
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRat.PackModel(null, model);
+				}
+			);
+		}
+
+		[Fact]
+		[Trait("Region", "PackRat methods")]
+		public void PackModel_ShouldWork()
+		{
+			PiedPiper piedPiper = new PiedPiper();
+			PackRat<long> packRat = new Int64PackRat(piedPiper);
+
+			PackRatTestHelper.TestPackModel<long>(packRat, 0, "00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000");
+			PackRatTestHelper.TestPackModel<long>(packRat, 1, "10000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000");
+			PackRatTestHelper.TestPackModel<long>(packRat, 2, "01000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000");
+			PackRatTestHelper.TestPackModel<long>(packRat, 3, "11000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000");
+			// ... other test cases ...
+
+			// Test negative values
+			PackRatTestHelper.TestPackModel<long>(packRat, -1, "11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111");
+			PackRatTestHelper.TestPackModel<long>(packRat, -2, "01111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111");
+			PackRatTestHelper.TestPackModel<long>(packRat, -3, "10111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111");
+			// ... other test cases ...
+		}
+
+		[Fact]
+		[Trait("Region", "PackRat methods")]
+		public void UnpackModel_ShouldThrowWhenReaderIsNull()
+		{
+			PiedPiper piedPiper = new PiedPiper();
+			Int64PackRat packRat = new Int64PackRat(piedPiper);
+
+			Assert.Throws<ArgumentNullException>(
+				() =>
+				{
+					packRat.UnpackModel(null);
+				}
+			);
+		}
+
+		[Fact]
+		[Trait("Region", "PackRat methods")]
+		public void UnpackModel_ShouldWork()
+		{
+			PiedPiper piedPiper = new PiedPiper();
+			PackRat<long> packRat = new Int64PackRat(piedPiper);
+
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 0);
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "10000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 1);
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "01000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 2);
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "11000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000", 3);
+			// ... other test cases ...
+
+			// Test negative values
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "11111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111", -1);
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "01111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111", -2);
+			PackRatTestHelper.TestUnpackModel<long>(packRat, "10111111 11111111 11111111 11111111 11111111 11111111 11111111 11111111", -3);
+			// ... other test cases ...
+		}
+		#endregion
+	}
+}
---
+++ BigRedProf.Data/Internal/PackRats/Int64PackRat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BigRedProf.Data.Internal.PackRats
+{
+	internal class Int64PackRat : PackRat<long>
+	{
+		#region constructos
+		public Int64PackRat(IPiedPiper piedPiper)
+			: base(piedPiper)
+		{
+		}
+		#endregion
+
+		#region PackRat methods
+		public override void PackModel(CodeWriter writer, long model)
+		{
+			if(writer == null)
+				throw new ArgumentNullException(nameof(writer));
+
+			Code code = new Code(BitConverter.GetBytes(model));
+			writer.WriteCode(code);
+		}
+
+		public override long UnpackModel(CodeReader reader)
+		{
+			if(reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			Code code = reader.Read(64);
+			long model = BitConverter.ToInt64(code.ByteArray, 0);
+			return model;
+		}
+		#endregion
+	}
+}
