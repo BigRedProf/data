041bb0fbdbb846573d2a687bc29cad6fa45e460a
Russell Froehlich <BigRedProf@outlook.com>
2024-06-11 09:47:29 -0700

    added equality logic to ModelWithSchema

--- BigRedProf.Data/ModelWithSchema.cs
+++ BigRedProf.Data/ModelWithSchema.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BigRedProf.Data
 {
@@ -8,7 +6,7 @@ namespace BigRedProf.Data
 	/// Represents a model with its schema. This can be useful when you store multiple models
 	/// together and won't otherwise know what schema each is.
 	/// </summary>
-	public class ModelWithSchema
+	public class ModelWithSchema : IEquatable<ModelWithSchema>
 	{
 		#region properties
 		/// <summary>
@@ -20,6 +18,58 @@ namespace BigRedProf.Data
 		/// The model.
 		/// </summary>
 		public object Model { get; set; }
+		#endregion
+
+		#region IEquatable<ModelWithSchema> methods
+		public bool Equals(ModelWithSchema other)
+		{
+			if (other == null)
+				return false;
+
+			if (SchemaId != other.SchemaId)
+				return false;
+
+			if (Model == null && other.Model != null)
+				return false;
+
+			if (Model != null && other.Model == null)
+				return false;
+
+			if (Model != null && !Model.Equals(other.Model))
+				return false;
+
+			return true;
+		}
+		#endregion
+
+		#region object methods
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as ModelWithSchema);
+		}
+
+		public override int GetHashCode()
+		{
+			int hashSchemaId = SchemaId == null ? 0 : SchemaId.GetHashCode();
+			int hashModel = Model == null ? 0 : Model.GetHashCode();
+			return hashSchemaId ^ hashModel;
+		}
+
+		public static bool operator ==(ModelWithSchema left, ModelWithSchema right)
+		{
+			if (object.ReferenceEquals(left, right))
+				return true;
+
+			if (object.ReferenceEquals(left, null) || object.ReferenceEquals(right, null))
+				return false;
+
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(ModelWithSchema left, ModelWithSchema right)
+		{
+			return !(left == right);
+		}
 		#endregion
 	}
 }
