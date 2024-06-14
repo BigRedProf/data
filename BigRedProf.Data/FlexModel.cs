84c14067e9fb962f3e0ff9f0b50cdd7ddf973eed
Russell Froehlich <BigRedProf@outlook.com>
2024-06-14 09:32:20 -0700

    implemented FlexModel
    Thank you, ChatGPT!!
    https://chatgpt.com/share/5f9a253a-456a-4175-b738-8b05f9235d43

--- BigRedProf.Data/FlexModel.cs
+++ BigRedProf.Data/FlexModel.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BigRedProf.Data
 {
 	/// <summary>
-	/// A flexible model that allows for a strong decoupling between producers or models
+	/// A flexible model that allows for a strong decoupling between producers of models
 	/// and consumers of models. Clients interact only with specific <see cref="Trait{M}"/>s,
 	/// so a consumer client may be able to understand some traits of a <see cref="FlexModel"/>
 	/// even if it doesn't understand all of its traits.
 	/// </summary>
 	public class FlexModel
 	{
-		#region methods
+		#region fields
+		private readonly Dictionary<string, object> _traits;
+		#endregion
 
+		#region constructors
+		public FlexModel()
+		{
+			_traits = new Dictionary<string, object>();
+		}
+		#endregion
+
+		#region methods
 		/// <summary>
 		/// Gets the list of trait identifiers.
 		/// </summary>
 		/// <returns>A list of trait identifiers.</returns>
 		public IList<string> GetTraitIds()
 		{
-			throw new NotImplementedException();
+			return _traits.Keys.ToList();
 		}
 
 		/// <summary>
@@ -29,7 +40,7 @@ namespace BigRedProf.Data
 		/// <returns>True if the trait exists, otherwise false.</returns>
 		public bool HasTrait(string traitIdentifier)
 		{
-			throw new NotImplementedException();
+			return _traits.ContainsKey(traitIdentifier);
 		}
 
 		/// <summary>
@@ -40,7 +51,17 @@ namespace BigRedProf.Data
 		/// <returns>The trait value.</returns>
 		public M GetTrait<M>(string traitIdentifier)
 		{
-			throw new NotImplementedException();
+			if (!_traits.TryGetValue(traitIdentifier, out var trait))
+				throw new ArgumentException($"Trait '{traitIdentifier}' does not exist.");
+
+			if (trait is Trait<M> typedTrait)
+			{
+				return typedTrait.Model;
+			}
+			else
+			{
+				throw new InvalidCastException($"Trait '{traitIdentifier}' is not of type {typeof(M)}.");
+			}
 		}
 
 		/// <summary>
@@ -52,7 +73,19 @@ namespace BigRedProf.Data
 		/// <returns>True if the trait exists, otherwise false.</returns>
 		public bool TryGetTrait<M>(string traitIdentifier, out M trait)
 		{
-			throw new NotImplementedException();
+			trait = default;
+			if (!_traits.TryGetValue(traitIdentifier, out var value))
+				return false;
+
+			if (value is Trait<M> typedTrait)
+			{
+				trait = typedTrait.Model;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 
 		/// <summary>
@@ -62,7 +95,10 @@ namespace BigRedProf.Data
 		/// <param name="trait">The trait to add.</param>
 		public void AddTrait<M>(Trait<M> trait)
 		{
-			throw new NotImplementedException();
+			if (trait == null)
+				throw new ArgumentNullException(nameof(trait));
+
+			_traits[trait.TraitId] = trait;
 		}
 
 		/// <summary>
@@ -72,9 +108,8 @@ namespace BigRedProf.Data
 		/// <returns>True if the trait was removed, otherwise false.</returns>
 		public bool RemoveTrait(string traitIdentifier)
 		{
-			throw new NotImplementedException();
+			return _traits.Remove(traitIdentifier);
 		}
-
 		#endregion
 	}
 }
