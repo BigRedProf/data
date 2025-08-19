e17caf7675fb1efacb716456a173d360d1cd1b3d
Russell Froehlich <BigRedProf@outlook.com>
2025-08-18 17:05:18 -0700

    added initial Librarian class

---
+++ src/Tape/Librarian.cs
@@ -0,0 +1,47 @@
+﻿using BigRedProf.Data.Core;
+using System;
+using System.Collections.Generic;
+
+namespace BigRedProf.Data.Tape
+{
+	abstract public class Librarian
+	{
+		#region fields
+		private IPiedPiper _piedPiper;
+		#endregion
+
+		#region protected constructors
+		protected Librarian(IPiedPiper piedPiper)
+		{
+			_piedPiper = piedPiper ?? throw new ArgumentNullException(nameof(piedPiper));
+			DefineCoreTraits();
+		}
+		#endregion
+
+		#region methods
+		public Tape FetchTape(Guid tapeId)
+		{
+			if (tapeId == Guid.Empty)
+				throw new ArgumentException("Tape ID cannot be empty.", nameof(tapeId));
+			
+			if (!TryFetchTape(tapeId, out Tape tape))
+				throw new KeyNotFoundException($"Tape with ID '{tapeId}' not found.");
+			
+			return tape;
+		}
+		#endregion
+
+		#region abstract methods
+		abstract public IEnumerable<Tape> FetchAllTapes();
+		abstract public IEnumerable<Tape> FetchTapesInSeries(Guid seriesId);
+		abstract public bool TryFetchTape(Guid tapeId, out Tape tape);
+		#endregion
+
+		#region private methods
+		private void DefineCoreTraits()
+		{
+			_piedPiper.DefineTrait(new TraitDefinition(TapeTrait.TapePosition, CoreSchema.Int32));
+		}
+		#endregion
+	}
+}
---
+++ src/Tape/TapeTrait.cs
@@ -0,0 +1,16 @@
+﻿namespace BigRedProf.Data.Tape
+{
+	/// <summary>
+	/// Contains the list of core trait identifiers.
+	/// </summary>
+	public static class TapeTrait
+	{
+		#region static fields
+		/// <summary>
+		/// Represents the current position in the tape, in bits. 0 is fully rewound, 1,000,000,000 is
+		/// fully fast-forwarded.
+		/// </summary>
+		public const string TapePosition = "bd25f6b9-628b-4f9c-8328-55d9a0c847f4";
+		#endregion
+	}
+}
