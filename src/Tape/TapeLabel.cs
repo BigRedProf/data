625596732243b7ecdeb5818f64fe7f6853dad3c7
Russell Froehlich <BigRedProf@outlook.com>
2025-09-21 16:48:31 -0700

    added more traits to TapeLabel

--- src/Tape/TapeLabel.cs
+++ src/Tape/TapeLabel.cs
@@ -22,6 +22,14 @@ namespace BigRedProf.Data.Tape
 
 		#region properties
 		public Guid TapeId => this.GetTrait<Guid>(CoreTrait.Id);
+		public string Name => this.GetTrait<string>(CoreTrait.Name);
+		public Multihash ContentMultihash => this.GetTrait<Multihash>(CoreTrait.ContentDigest);
+		public Guid SeriesId => this.GetTrait<Guid>(CoreTrait.SeriesId);
+		public string SeriesName => this.GetTrait<string>(CoreTrait.SeriesName);
+		public int SeriesNumber => this.GetTrait<int>(CoreTrait.SeriesNumber);
+		public Multihash SeriesParentMultihash => this.GetTrait<Multihash>(CoreTrait.SeriesParentDigest);
+		public Multihash SeriesHeadMultihash => this.GetTrait<Multihash>(CoreTrait.SeriesHeadDigest);
+		public int TapePosition => this.GetTrait<int>(TapeTrait.TapePosition);
 		#endregion
 
 		#region methods
@@ -29,6 +37,15 @@ namespace BigRedProf.Data.Tape
 		{
 			TapeLabel tapeLabel = new TapeLabel(this);
 			tapeLabel.AddTrait(new Trait<Guid>(CoreTrait.Id, id));
+			return tapeLabel;
+		}
+
+		public TapeLabel WithSeriesInfo(Guid seriesId, string seriesName, int seriesNumber)
+		{
+			TapeLabel tapeLabel = new TapeLabel(this);
+			tapeLabel.AddTrait(new Trait<Guid>(CoreTrait.SeriesId, seriesId));
+			tapeLabel.AddTrait(new Trait<string>(CoreTrait.SeriesName, seriesName));
+			tapeLabel.AddTrait(new Trait<int>(CoreTrait.SeriesNumber, seriesNumber));
 			return tapeLabel;
 		}
 		#endregion
