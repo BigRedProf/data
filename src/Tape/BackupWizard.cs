e577ca49c15c965fa95c4d9f3fceb4a33385a4bd
Russell Froehlich <BigRedProf@outlook.com>
2025-10-18 07:56:31 -0700

    added first draft of a BackupWizard

---
+++ src/Tape/BackupWizard.cs
@@ -0,0 +1,38 @@
+﻿using BigRedProf.Data.Core;
+using System;
+
+namespace BigRedProf.Data.Tape
+{
+	public class BackupWizard
+	{
+		#region fields
+		private Librarian _librarian;
+		private Guid _seriesId;
+		#endregion
+
+		#region constructors
+		public BackupWizard(Librarian librarian, Guid seriesId)
+		{
+			_librarian = librarian;
+			_seriesId = seriesId;
+		}
+		#endregion
+
+		#region methods
+		public Code GetLatestCheckpoint()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetLatestCheckpoint(Code clientCheckpointCode)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Record(Code content)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
