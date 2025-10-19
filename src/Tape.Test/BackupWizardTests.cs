3f4b80ef4d16581b792c3d893f825882642ef4c0
Russell Froehlich <BigRedProf@outlook.com>
2025-10-18 17:02:00 -0700

    added skeleton BackupWizard tests file

---
+++ src/Tape.Test/BackupWizardTests.cs
@@ -0,0 +1,24 @@
+﻿using BigRedProf.Data.Core;
+using BigRedProf.Data.Tape._TestHelpers;
+
+namespace BigRedProf.Data.Tape.Test
+{
+	public class BackupWizardTests
+	{
+		#region constructors
+		public BackupWizardTests()
+		{
+		}
+		#endregion
+
+		#region unit tests
+		[Trait("Region", "Librarian methods")]
+		public void MainScenario_ShouldWork()
+		{
+			TapeProvider tapeProvider = TapeProviderHelper.CreateMemoryTapeProvider();
+
+			// TODO: implement a classic backup case
+		}
+		#endregion
+	}
+}
